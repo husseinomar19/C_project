74984907b29c178f0494c4490f1384ed224cc89f
Hussein Omar <118814567+husseinomar19@users.noreply.github.com>
2024-02-26 18:58:11 +0100

    Add files via upload

---
+++ Eerstproject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Eerstproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
